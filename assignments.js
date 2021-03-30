29189095f461c9876ce52ec5bfd5fb1c00b9728f
William Newton <81594388+KarmaIDK@users.noreply.github.com>
2021-03-30 15:54:08 -0400

    Update assignments.js

--- assignments.js
+++ assignments.js
@@ -1,8 +1,8 @@
 let Country = 'USA';
 let Continent = 'North America';
-let Population = '326 Million'
+let Population = '326 Million';
 
-console.log(Country)
-console.log(Continent)
-console.log(Population)
+console.log(Country);
+console.log(Continent);
+console.log(Population);
 
